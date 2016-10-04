a257a3094126d4eaa40d12724c19fe2b42c78e19
Jonathan Cabas <jcabas.ing@gmail.com>
2016-10-04 14:25:11 -0500

    Adding tests to person model

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  should validate_length_of(:first_name).is_at_most(75)
+  should validate_presence_of(:first_name)
+
+  should validate_length_of(:last_name).is_at_most(75)
+  should validate_presence_of(:last_name)
+
+  should validate_length_of(:email).is_at_most(254)
+  should validate_presence_of(:email)
+  should validate_uniqueness_of(:email)
+  should allow_value('test@gap.com').for(:email)
+  should_not allow_value("test").for(:email)
+
+  should validate_length_of(:job).is_at_most(75)
+  should allow_value(nil).for(:job)
+
+  should validate_length_of(:bio).is_at_most(300)
+  should allow_value(nil).for(:bio)
+
+  should validate_presence_of(:gender)
+  should validate_inclusion_of(:gender).in_array(Person::GENDER)
+
+  should validate_presence_of(:birthdate)
+  should allow_value((Date.today - 29.years)).for(:birthdate)
+
+  def test_validation_on_birthdate
+    person = Person.new(birthdate: Date.today + 1.day)
+    person.valid?
+    assert_equal "is not a valid date", person.errors[:birthdate][0]
+  end
 end
